b655519a3857474633fc53fb4fb4b707b20287e7
kosanaanitha <kosanaanitha123@gmail.com>
2025-10-29 13:03:50 +0530

    Add files via upload

---
+++ data types.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int x=34;
+	printf("x=%d",x);
+	float p=2.5;
+	printf("\np=%f",p);
+	char ch='a';
+	printf("\nch=%c",ch);
+	long int l=235678;
+	printf("\nl=%ld",l);
+	double d=239.701;
+	printf("\nd=%lf",d);
+	return 0;
+	
+}
