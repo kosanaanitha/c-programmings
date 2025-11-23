61e6a57aea2720b202539bbcc05bc190aa3f7d0f
kosanaanitha <kosanaanitha123@gmail.com>
2025-11-23 18:25:57 +0530

    Add files via upload

---
+++ string handling functions (length).cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char s[20];
+	int l;
+	printf("enter string name");
+	scanf("%s",s);
+	printf("length of the string=%d",strlen(s));
+	return 0;
+}
